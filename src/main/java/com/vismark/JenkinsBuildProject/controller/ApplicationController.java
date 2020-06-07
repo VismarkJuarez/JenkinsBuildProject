47a7d3dc913f80cd356c9fcee010ff1655640c75
Vismark Juarez <vismarkjuarez@gmail.com>
2020-06-06 23:56:58 -0500

    Modifying service port.

---
+++ src/main/java/com/vismark/JenkinsBuildProject/controller/ApplicationController.java
@@ -0,0 +1,18 @@
+package com.vismark.JenkinsBuildProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+    
+    /**
+     * A test endpoint to ensure the application is running adequately.
+     * @return
+     */
+    @RequestMapping(method=RequestMethod.GET, value="/liveness")
+	public String testingHeaders() {
+		return "all good, brah.";
+	}
+}
