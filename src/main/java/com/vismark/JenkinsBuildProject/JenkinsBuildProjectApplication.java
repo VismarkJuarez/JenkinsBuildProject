9fe752c0ec1326d86009f978fc9b32fdf39d6791
Vismark Juarez <vismarkjuarez@gmail.com>
2020-06-06 23:41:31 -0500

    Initializing repository.

---
+++ src/main/java/com/vismark/JenkinsBuildProject/JenkinsBuildProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vismark.JenkinsBuildProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBuildProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vismark/JenkinsBuildProject/JenkinsBuildProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vismark.JenkinsBuildProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuildProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
